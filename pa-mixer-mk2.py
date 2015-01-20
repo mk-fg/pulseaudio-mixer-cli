6838e247a1b804f54a7e6429988695a68390d954
Mike Kazantsev <mk.fraggod@gmail.com>
2015-01-20 21:30:38 +0500

    pa-mixer-mk2: some more copy-pasting

--- pa-mixer-mk2.py
+++ pa-mixer-mk2.py
@@ -31,9 +31,31 @@ class PAMixerGlibLoop(object):
 		Should ideally be started as soon as possible,
 			to avoid carrying whatever baggage from parent pid after fork.'''
 
-
 	def __init__(self, sig=signal.SIGUSR1):
-		self.signal = sig
+		self.signal, self._pipe = sig, None
+
+
+	def child_start(self):
+		signal.signal(self.signal, signal.SIG_IGN)
+		if self._pipe: self._pipe.close()
+		fd_out, fd_in = os.pipe()
+		self.core_pid = os.getpid()
+		self.child_pid = os.fork()
+
+		if not self.child_pid:
+			try: self._child_run() # returns only on errors
+			except Exception as err:
+				log.exception('Unexpectedly broke out of glib loop due to exception: %s', err)
+			raise PAMixerGlibLoopError()
+
+		else:
+			os.close(fd_in)
+			self._pipe = io.open(fd_out, 'rb', buffering=0)
+
+	def child_check_restart(self):
+		if os.waitpid(child_pid, os.WNOHANG)[0]:
+			log.debug('glib/dbus child pid died. restarting it')
+			self.child_start()
 
 
 	def _get_bus_address(self):
@@ -64,24 +86,6 @@ class PAMixerGlibLoop(object):
 				# sleep(1) # XXX: still needed?
 				srv_addr = False # to avoid endless loop
 		return self._dbus.connection.Connection(srv_addr)
-
-
-	def start(self):
-		signal.signal(self.signal, signal.SIG_IGN)
-		fd_out, fd_in = os.pipe()
-		self.core_pid = os.getpid()
-		self.child_pid = os.fork()
-
-		if not self.child_pid:
-			try: self._child_run() # returns only on errors
-			except Exception as err:
-				log.exception('Unexpectedly broke out of glib loop due to exception: %s', err)
-			raise PAMixerGlibLoopError()
-
-		else:
-			os.close(fd_in)
-			self._pipe = io.open(fd_out, 'rb', buffering=0)
-
 
 	def _core_notify(self, path, op):
 		try:
@@ -123,13 +127,105 @@ class PAMixerGlibLoop(object):
 
 
 
-class PAMixerUIUpdate(Exception): pass
+class PAMixerUIUpdate(Exception): pass # XXX: not needed here?
 
 class PAMixerUI(object): pass
 
+	item_len_min = 10
+	bar_len_min = 10
+	bar_caps_func = lambda bar='': ' [ ' + bar + ' ]'
+
+	def __init__(self): pass
 
 
+	# All things curses
 
+	def c_win_init(self):
+		win = self.c.newwin(*self.c_win_size())
+		win.keypad(True)
+		return win
+
+	def c_win_size(self, border):
+		'Returns "nlines, ncols, begin_y, begin_x" for e.g. newwin(), taking border into account.'
+		size = self.c_stdscr.getmaxyx()
+		nlines, ncols = max(1, size[0] - 2 * border), max(1, size[1] - 2 * border)
+		return nlines, ncols, min(border, size[0]), min(border, size[1])
+
+	def c_win_draw(self, win, items, hl=None):
+		win_rows, win_len = win.getmaxyx()
+		if win_len <= 1: return
+
+		item_len_max = items.max_key_len
+		mute_button_len = 2
+		bar_len = win_len - item_len_max - mute_button_len - len(self.bar_caps_func())
+		if bar_len < self.bar_len_min:
+			item_len_max = max(self.item_len_min, item_len_max + bar_len - self.bar_len_min)
+			bar_len = win_len - item_len_max - mute_button_len - len(self.bar_caps_func())
+			if bar_len <= 0: item_len_max = win_len # just draw labels
+			if self.item_len_max < self.item_len_min: item_len_max = min(items.max_key_len, win_len)
+
+		win.erase() # cleanup old entries
+		for row, item in enumerate(items):
+			if row >= win_rows - 1: break # not sure why bottom window row seem to be unusable
+
+			attrs = self.c.A_REVERSE if item == hl else self.c.A_NORMAL
+
+			win.addstr(row, 0, item[:item_len_max].encode(optz.encoding), attrs)
+			if win_len > item_len_max + mute_button_len:
+				if items.get_mute(item): mute_button = " M"
+				else: mute_button = " -"
+				win.addstr(row, item_len_max, mute_button)
+
+				if bar_len > 0:
+					bar_fill = int(round(items.get_volume(item) * bar_len))
+					bar = self.bar_caps_func('#' * bar_fill + '-' * (bar_len - bar_fill))
+					win.addstr(row, item_len_max + mute_button_len, bar)
+
+
+	def _run(self, stdscr, items, border):
+		c, self.c_stdscr = self.c, stdscr
+
+		c.curs_set(0)
+		c.use_default_colors()
+
+		win = self.c_win_init()
+		hl = next(iter(items)) if items else '' # XXX: still use something like items object?
+		optz.adjust_step /= 100.0
+
+		while True:
+			self.child_check_restart()
+
+			while items.updates: items.update()
+			if not items: items.refresh()
+			try: self.c_win_draw(win, items, hl=hl)
+			except PAMixerUIUpdate: continue
+			if items.updates: continue
+
+			try: key = win.getch()
+			except c.error: continue
+			log.debug('Keypress event: %s', key)
+
+			try:
+				if key in (c.KEY_DOWN, ord('j'), ord('n')): hl = items.next_key(hl)
+				elif key in (c.KEY_UP, ord('k'), ord('p')): hl = items.prev_key(hl)
+				elif key in (c.KEY_LEFT, ord('h'), ord('b')):
+					items.set_volume(hl, items.get_volume(hl) - optz.adjust_step)
+				elif key in (c.KEY_RIGHT, ord('l'), ord('f')):
+					items.set_volume(hl, items.get_volume(hl) + optz.adjust_step)
+				elif key in (ord(' '), ord('m')):
+					items.set_mute(hl, not items.get_mute(hl))
+				elif key < 255 and key > 0 and chr(key) == 'q': sys.exit(0)
+				elif key in (c.KEY_RESIZE, ord('\f')):
+					c.endwin()
+					stdscr.refresh()
+					win = self.c_win_init()
+			except PAMixerUIUpdate: continue
+
+
+	def run(self, stdscr, items, border=1):
+		import curses # has a ton of global state
+		self.c = curses
+		self.c.wrapper(self._run, items=items, border=border)
 
 
 
@@ -171,8 +267,6 @@ def main(args=None):
 	except (OSError, IOError) as err:
 		log.debug('Not processing config file %r: %s %s', conf_file, type(err), err)
 	else: update_conf_from_file(conf, conf_file)
-
-
 
 
 
