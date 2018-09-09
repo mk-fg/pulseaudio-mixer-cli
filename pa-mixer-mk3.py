b6c9bb1b935d73abf9ee886b165a88ff03d84e8e
Mike Kazantsev <mk.fraggod@gmail.com>
2018-09-09 12:21:31 +0500

    mk3: add "Playback" to list of generic media.name values

--- pa-mixer-mk3.py
+++ pa-mixer-mk3.py
@@ -50,7 +50,8 @@ class Conf:
 	use_device_name = False
 	use_media_name = False
 	placeholder_media_names = [ # avoid displaying these, as they're not informative
-		'audio stream', 'AudioStream', 'Output', 'ALSA Playback', 'Simple DirectMedia Layer' ]
+		'audio stream', 'AudioStream', 'Output', 'Playback',
+		'ALSA Playback', 'Simple DirectMedia Layer' ]
 	name_len_max = 100
 	name_cut_from = 'left' # "left" or "right"
 	name_show_level = True
